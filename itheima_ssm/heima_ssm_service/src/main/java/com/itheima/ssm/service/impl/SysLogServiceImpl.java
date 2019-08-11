982bfb7f051a4a75931e497abcd4ff7086ed0713
qiangdadalz <1464721051@qq.com>
2019-08-11 11:35:40 +0800

    完成访问日志、添加日志功能

---
+++ itheima_ssm/heima_ssm_service/src/main/java/com/itheima/ssm/service/impl/SysLogServiceImpl.java
@@ -0,0 +1,34 @@
+package com.itheima.ssm.service.impl;
+
+import com.itheima.ssm.dao.ISysLogDao;
+import com.itheima.ssm.domain.SysLog;
+import com.itheima.ssm.service.ISysLogService;
+import com.itheima.ssm.utils.UuidUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+@Transactional
+public class SysLogServiceImpl implements ISysLogService {
+
+    @Autowired
+    private ISysLogDao sysLogDao;
+
+    UuidUtils uuidUtils = new UuidUtils();
+
+    @Override
+    public List<SysLog> findAll() throws Exception {
+        return sysLogDao.findAll();
+    }
+
+    @Override
+    public void save(SysLog sysLog) throws Exception {
+
+        sysLog.setId(uuidUtils.makeUuid());
+
+        sysLogDao.save(sysLog);
+    }
+}
